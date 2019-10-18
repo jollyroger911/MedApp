9a72a4afbb6cdedb8c27cad796ab68ac04476cc6
jollyroger911 <anton.klim911@gmail.com>
2019-10-18 13:29:02 +0300

    README.md changed

---
+++ src/test/java/by/sam/apklimovich/SampleIntegrationTest.java
@@ -0,0 +1,12 @@
+package by.sam.apklimovich;
+
+import jdk.jfr.Category;
+import org.junit.Test;
+
+
+public class SampleIntegrationTest {
+    @Test
+    public void sampleIntegrationTest(){
+        System.out.println("IntegrationTest");
+    }
+}
