94c83eff64960d9fe81851037e9d6b2e153bca0a
jollyroger911 <anton.klim911@gmail.com>
2019-10-25 12:34:18 +0300

    BOM added.

--- src/main/java/by/sam/apklimovich/HelloController.java
+++ src/main/java/by/sam/apklimovich/HelloController.java
@@ -1,21 +1,10 @@
 package by.sam.apklimovich;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import sun.jvm.hotspot.HelloWorld;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Locale;
-
-//@Controller
-//public class HelloController {
-//    @RequestMapping("/")
-//    public String display()
-//    {
-//        return "index";
-//    }
-//}
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
--- src/main/java/by/sam/apklimovich/WebMvcConfig.java
+++ src/main/java/by/sam/apklimovich/WebMvcConfig.java
@@ -4,7 +4,7 @@ package by.sam.apklimovich;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import sun.jvm.hotspot.HelloWorld;
+
 
 @Configuration
 //@EnableWebMvc
