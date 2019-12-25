b6972ba483ac92c04c92f71858c27da03cd27336
Klimovich <anton.klim911@gmail.com>
2019-12-25 18:22:45 +0300

    Merge branch 'Thymeleaf' of C:\Users\Klimovich\IdeaProjects\MedApp with conflicts.

--- src/main/java/by/sam/apklimovich/controllers/ChatController.java
+++ src/main/java/by/sam/apklimovich/controllers/ChatController.java
@@ -31,7 +31,6 @@ import javax.validation.Valid;
 @Controller
 public class ChatController {
 
-    @Autowired
     public PersonService personService;
 
     public MessageService messageService;
@@ -54,14 +53,12 @@ public class ChatController {
     public String chat(Model model) {
         model.addAttribute("chatMeas", new MessageDto());
         model.addAttribute("chatDto", chat);
-        String str = personService.getPersonsNameById(personDto.getDestId());
-        model.addAttribute("destName", str);
         this.chat.setSenderId(personDto.getId());
         this.chat.setRecieverId(personDto.getDestId());
         chatService.createChat(chat, personDto);
       //  model.addAttribute("receiverData", personService.getPersonsDataForChat(personDto.getDestId()));
         model.addAttribute("messagesList", chat.getCurrentMessages());
-        return "chat_three";
+        return "chat_two";
     }
 
 //    @RequestMapping(value = "/chat", method = RequestMethod.POST)
